89123ef04aa16c83d4ba07607e0410f78900bccc
Biruk tesfa <biruktesfa444@gmail.com>
2026-06-07 18:42:44 +0300

    feat(repository): add UserRepository with email queries

---
+++ src/main/java/com/eduflow/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.eduflow.repository;
+
+import com.eduflow.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
